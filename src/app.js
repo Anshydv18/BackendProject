d44084f2e5fae1fb10f6499e9c5108dbd7d278fe
Anshydv18 <anshgithub@gmail.com>
2024-05-28 14:18:59 +0530

    save

--- src/app.js
+++ src/app.js
@@ -15,4 +15,13 @@ app.use(expres.urlencoded({extended:true,limit:"16kb"}))
 app.use(expres.static("public"))
 app.use(cookieParser())
 
+
+
+//routes import
+
+import userRouter from './routers/user.routers.js'
+
+ //routes declaration
+ app.use('/api/v1/user',userRouter)
+
 export { app }
